1ae9125067b387f138adf6789e8e5ee7fc2dcff3
CaigerDeng <416715486@qq.com>
2020-02-21 19:43:24 +0800

    add TwoSum

---
+++ LeetCodeNote/LeetCodeNote/Array/001_TwoSum.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 1. 两数之和: https://leetcode-cn.com/problems/two-sum/
+/// </summary>
+
+namespace LeetCodeNote.Array
+{
+    public class Solution
+    {
+
+        //solution 0
+        public int[] TwoSum_0(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == target - nums[i])
+                    {
+                        return new[] { i, j };
+                    }
+
+                }
+
+            }
+            return null;
+        }
+
+        //solution 1 
+        public int[] TwoSum_1(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int key = nums[i];
+                if (!dic.ContainsKey(key))
+                {
+                    dic.Add(key, i);
+                }
+                dic[key] = i;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int left = target - nums[i];
+                if (dic.ContainsKey(left) && dic[left] != i)
+                {
+                    return new[] { i, dic[left] };
+                }
+            }
+            return null;
+        }
+
+        //solution 2 
+        public int[] TwoSum_2(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int key = nums[i];
+                int left = target - key;
+                // can remove '&& dic[left] != i'
+                if (dic.ContainsKey(left))
+                {
+                    return new[] { i, dic[left] };
+                }
+                if (!dic.ContainsKey(key))
+                {
+                    dic.Add(key, i);
+                }
+                dic[key] = i;
+            }           
+            return null;
+        }
+
+    }
+
+}
---
+++ LeetCodeNote/LeetCodeNote/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeNote.Array;
+
+namespace LeetCodeNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution so = new Solution();
+            int[] arr = new int[] { 3, 3 };
+            int[] res = so.TwoSum_2(arr, 6);
+            
+
+            PrintArr(res);
+
+
+            Console.ReadLine();
+
+        }
+
+
+        static void PrintArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ", ");
+            }
+            Console.WriteLine("\n");
+
+        }
+
+
+
+
+
+
+    }
+
+    
+
+}
