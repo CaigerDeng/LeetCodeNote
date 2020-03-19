85ed95db239ae70353d3377a191cdeccd30a7895
CaigerDeng <416715486@qq.com>
2020-03-19 10:38:43 +0800

    add 566. matrix reshape

---
+++ LeetCodeNote/LeetCodeNote/Array/566_MatrixReshape.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 566. 重塑矩阵
+    /// https://leetcode-cn.com/problems/reshape-the-matrix/
+    /// </summary>
+
+    public class Solution_566
+    {
+        // method 0 
+        public int[][] MatrixReshape_0(int[][] nums, int r, int c)
+        {
+            // int[][]交错数组
+            // int[][] arr = new int[5][];
+            // arr[0] = new int[] { 1, 2 };
+            // arr[1] = new int[] { 3, 4, 5 };
+            // Console.WriteLine("{0}, {1}, {2}", arr.Length, arr[0].Length, arr[1].Length); // 输出：5, 2, 3 
+
+            if (nums.Length == 0 || r * c != nums.Length * nums[0].Length)
+            {
+                return nums;
+            }
+            int[][] res = new int[r][];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = new int[c];
+            }
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums[0].Length; j++)
+                {
+                    queue.Enqueue(nums[i][j]);
+                }
+            }
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    res[i][j] = queue.Dequeue();
+                }
+            }
+            return res;
+        }
+
+        // method 1
+        // 对method 0优化了队列
+        public int[][] MatrixReshape_1(int[][] nums, int r, int c)
+        {
+            if (nums.Length == 0 || r * c != nums.Length * nums[0].Length)
+            {
+                return nums;
+            }
+            int[][] res = new int[r][];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = new int[c];
+            }
+            int rows = 0;
+            int cols = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums[0].Length; j++)
+                {
+                    res[rows][cols] = nums[i][j];
+                    cols++;
+                    if (cols == c)
+                    {
+                        rows++;
+                        cols = 0;
+                    }
+                }
+            }
+            return res;
+        }
+
+        // method 2
+        // 对method 1优化了行列检查
+        public int[][] MatrixReshape_2(int[][] nums, int r, int c)
+        {
+            if (nums.Length == 0 || r * c != nums.Length * nums[0].Length)
+            {
+                return nums;
+            }
+            int[][] res = new int[r][];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = new int[c];
+            }
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums[0].Length; j++)
+                {
+                    res[count / c][count % c] = nums[i][j];
+                    count++;
+                }
+            }
+            return res;
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
--- LeetCodeNote/LeetCodeNote/Program.cs
+++ LeetCodeNote/LeetCodeNote/Program.cs
@@ -11,13 +11,27 @@ namespace LeetCodeNote
     {
         static void Main(string[] args)
         {
-            Solution_532 so = new Solution_532();
+            Solution_566 so = new Solution_566();
             int[] nums1 = new[] { 1, 3, 1, 5, 4 };
 
             //int[] nums2 = { 2, 5, 6 };
 
 
-            Console.WriteLine("res:{0}", so.FindPairs_1(nums1, 0));
+            //            int[,] nums = new int[,] { {1, 2}, {3, 4}, {5, 6} };
+            //            Console.WriteLine("{0}, {1}, {2}", nums.Length, nums.GetLength(0), nums.GetLength(1));
+
+            int[][] arr = new int[2][];
+            arr[0] = new int[] { 1, 2 };
+            arr[1] = new int[] { 3, 4 };
+
+
+            //arr[1] = new int[] { 3, 4 };
+            //Console.WriteLine("{0}, {1}, {2}", arr.Length, arr[0].Length, arr[1].Length);
+
+
+
+
+            Console.WriteLine("res:{0}", so.MatrixReshape_0(arr, 2, 2));
 
 
 
