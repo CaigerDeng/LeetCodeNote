738c8f0d4cc076601827ad72336fd00f156a2afa
CaigerDeng <416715486@qq.com>
2020-04-02 14:52:19 +0800

    add 724. pivot index

---
+++ LeetCodeNote/LeetCodeNote/Array/724_PivotIndex.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 724. 寻找数组的中心索引
+    /// https://leetcode-cn.com/problems/find-pivot-index/
+    /// </summary>
+
+    public class Solution_724
+    {
+        // method 0
+        public int PivotIndex(int[] nums)
+        {
+            int sum = 0;
+            int leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (leftSum == sum - leftSum - nums[i])
+                {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+            return -1;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
