5ecdabe158e77e580e56edda16170b149c61b755
CaigerDeng <416715486@qq.com>
2020-04-07 10:35:43 +0800

    add 747. dominant index

---
+++ LeetCodeNote/LeetCodeNote/Array/746_MinCostClimbingStairs.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 746. 使用最小花费爬楼梯
+    /// https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+    /// </summary>
+
+
+    public class Solution_746
+    {
+        // method 0
+        // ...暂未看懂
+        public int MinCostClimbingStairs_0(int[] cost)
+        {
+
+
+            return 0;
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ LeetCodeNote/LeetCodeNote/Array/747_DominantIndex.cs
@@ -0,0 +1,71 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 747. 至少是其他数字两倍的最大数
+    /// https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/
+    /// </summary>
+
+    public class Solution_747
+    {
+        // method 0
+        // mine：找到最大的两个数，如果第一大的数比第二大的数的2倍还大，则为true
+        public int DominantIndex_0(int[] nums)
+        {
+            // max1 > max2
+            int max1 = int.MinValue;
+            int max2 = int.MinValue;
+            int index = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int val = nums[i];
+                if (val >= max1)
+                {
+                    max2 = max1;
+                    max1 = val;
+                    index = i;
+                }
+                else if (val >= max2)
+                {
+                    max2 = val;
+                }
+            }
+            if (max1 < max2 * 2)
+            {
+                index = -1;
+            }
+            return index;
+        }
+
+        // method 1
+        public int DominantIndex_1(int[] nums)
+        {
+            int maxIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
+                {
+                    return -1;
+                }
+            }
+            return maxIndex;
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
