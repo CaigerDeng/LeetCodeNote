13f81e2d1fbf6b2723735b91b122107dbc07edb0
CaigerDeng <416715486@qq.com>
2020-04-10 12:37:11 +0800

    add 830. large group position

---
+++ LeetCodeNote/LeetCodeNote/Array/830_LargeGroupPositions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 830. 较大分组的位置
+    /// https://leetcode-cn.com/problems/positions-of-large-groups/
+    /// </summary>
+    /// 
+    public class Solution_830
+    {
+        // method 0
+        public IList<IList<int>> LargeGroupPositions_0(string S)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            int i = 0;
+            int len = S.Length;
+            for (int j = 0; j < len; j++)
+            {
+                if (j == len - 1 || S[j] != S[j + 1])
+                {
+                    if (j - i + 1 >= 3)
+                    {
+                        List<int> list = new List<int>();
+                        list.Add(i);
+                        list.Add(j);
+                        res.Add(list);
+                    }
+                    i = j + 1;
+                }
+            }
+            return res;
+        }
+
+
+
+
+    }
+}
--- LeetCodeNote/LeetCodeNote/Program.cs
+++ LeetCodeNote/LeetCodeNote/Program.cs
@@ -11,15 +11,16 @@ namespace LeetCodeNote
     {
         static void Main(string[] args)
         {
-            Solution_665 so = new Solution_665();
+            Solution_830 so = new Solution_830();
             int[] nums1 = new[] { 5, 2, 7, 6 };
 
-            int[][] arr = new int[5][]; // 5代表有arr[0]，arr[1]，arr[2]，arr[3]，arr[4]
-            arr[0] = new int[] { 1, 2, 3, 4 };
-            arr[1] = new int[] { 3, 4, 5 };
-            Console.WriteLine("{0}, {1}, {2}", arr.Length, arr[0].Length, arr[1].Length); // 输出：5, 4, 3 
+            //int[][] arr = new int[5][]; // 5代表有arr[0]，arr[1]，arr[2]，arr[3]，arr[4]
+            //arr[0] = new int[] { 1, 2, 3, 4 };
+            //arr[1] = new int[] { 3, 4, 5 };
+            //Console.WriteLine("{0}, {1}, {2}", arr.Length, arr[0].Length, arr[1].Length); // 输出：5, 4, 3 
 
 
+            Console.WriteLine(so.LargeGroupPositions_0("aaa"));
 
             Console.ReadLine();
 
