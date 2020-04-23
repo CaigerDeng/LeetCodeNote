a0e59f8dbdc655c13f13c8d6663e2ba374d4ed74
CaigerDeng <416715486@qq.com>
2020-04-23 11:22:47 +0800

    add 867. transpose

---
+++ LeetCodeNote/LeetCodeNote/Array/867_Transpose.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 867. 转置矩阵
+    /// https://leetcode-cn.com/problems/transpose-matrix/
+    /// </summary>
+
+    public class Solution_867
+    {
+        // method 0
+        public int[][] Transpose_0(int[][] A)
+        {
+            int R = A.Length;
+            int C = A[0].Length;
+            int[][] res = new int[C][];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = new int[R];
+            }
+            for (int r = 0; r < R; r++)
+            {
+                for (int c = 0; c < C; c++)
+                {
+                    res[c][r] = A[r][c];
+                }             
+            }
+            return res;
+        }
+
+
+
+
+    }
+
+
+}
