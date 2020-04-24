d9af5d9a80e56a7d6952da1f4eafbffecbcbf879
CaigerDeng <416715486@qq.com>
2020-04-24 10:33:26 +0800

    add 941. valid mountain array

---
+++ LeetCodeNote/LeetCodeNote/Array/941_ValidMountainArray.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 941. 有效的山脉数组
+    /// https://leetcode-cn.com/problems/valid-mountain-array/
+    /// </summary>
+
+    public class Solution_941
+    {
+
+        // method 0 
+        public bool ValidMountainArray_0(int[] A)
+        {
+            int N = A.Length;
+            int i = 0;
+            while (i + 1 < N && A[i] < A[i + 1])
+            {
+                i++;
+            }
+            if (i == 0 || i == N - 1)
+            {
+                return false;
+            }
+            while (i + 1 < N && A[i] > A[i + 1])
+            {
+                i++;
+            }
+            return i == N - 1;
+        }
+
+
+    }
+
+
+}
