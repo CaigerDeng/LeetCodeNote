125eb6cbb338640cd43f19e8842e6b0c501f5d0b
CaigerDeng <416715486@qq.com>
2020-08-03 13:48:46 +0800

    add 1365. smaller numbers than current

---
+++ LeetCodeNote/LeetCodeNote/Array/1365_SmallerNumbersThanCurrent.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1365. 有多少小于当前数字的数字
+    /// https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    /// </summary>
+
+    public class Solution_1365
+    {
+        // method 0 
+        // 暴力Linq用法
+        public int[] SmallerNumbersThanCurrent_0(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = nums.Count(item => item < nums[i]);
+            }
+            return res;
+
+        }
+
+        // method 1
+        // 频次数组 + 前缀和
+        public int[] SmallerNumbersThanCurrent_1(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            int[] cnt = new int[101];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                cnt[nums[i]]++;
+            }
+            // 求前缀和
+            for (int i = 1; i <= 100; i++)
+            {
+                cnt[i] += cnt[i - 1];
+            }
+            for (int i = 0; i < res.Length; i++)
+            {
+                int val = nums[i] - 1;
+                if (val >= 0)
+                {
+                    res[i] = cnt[val];
+                }                
+            }
+            return res;
+
+        }
+
+        // method 2
+        // 排序
+        public int[] SmallerNumbersThanCurrent_2(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            nums.CopyTo(res, 0);
+            System.Array.Sort(res);
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < res.Length; i++)
+            {
+                int k = res[i];
+                int v = i;
+                if (!dic.ContainsKey(k))
+                {
+                    dic.Add(k, v);
+                }                
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int k = nums[i];
+                res[i] = dic[k];
+            }
+            return res;
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
--- LeetCodeNote/LeetCodeNote/Program.cs
+++ LeetCodeNote/LeetCodeNote/Program.cs
@@ -12,18 +12,18 @@ namespace LeetCodeNote
     {
         static void Main(string[] args)
         {
-            Solution_1351 so = new Solution_1351();
+            Solution_1365 so = new Solution_1365();
             // arr1 = {2,3,1,3,2,4,6,7,9,2,19}
-            //            int[] arr1 = new int[] { 26, 21, 11, 20, 50, 34, 1, 18 };
+            int[] arr1 = new int[] { 5, 0, 10, 0, 10, 6 };
             //int[] arr2 = new int[] { 21, 11, 26, 20 };
 
 
-            int[][] arr = new int[5][];
-            arr[0] = new int[] { 4, 3, 3, 1, 1 };
-            arr[1] = new int[] { 1, 0, 0, -1, -1 };
-            arr[2] = new int[] { -2, -2, -2, -2, -3 };
-            arr[3] = new int[] { -2, -2, -2, -2, -3 };
-            arr[4] = new int[] { -3, -3, -3, -3, -3 };
+            //int[][] arr = new int[5][];
+            //arr[0] = new int[] { 4, 3, 3, 1, 1 };
+            //arr[1] = new int[] { 1, 0, 0, -1, -1 };
+            //arr[2] = new int[] { -2, -2, -2, -2, -3 };
+            //arr[3] = new int[] { -2, -2, -2, -2, -3 };
+            //arr[4] = new int[] { -3, -3, -3, -3, -3 };
 
             //string[] que = new string[] { "bba", "abaaaaaa", "aaaaaa", "bbabbabaab", "aba", "aa", "baab", "bbbbbb", "aab", "bbabbaabb" };
             //string[] word = new string[] { "aaabbb", "aab", "babbab", "babbbb", "b", "bbbbbbbbab", "a", "bbbbbbbbbb", "baaabbaab", "aa" };
@@ -34,7 +34,7 @@ namespace LeetCodeNote
             //            grid[2] = new int[] { 7, 8, 9 };
 
 
-            int res = so.CountNegatives_2(arr);
+            PrintArr(so.SmallerNumbersThanCurrent_1(arr1));
 
             Console.WriteLine();
 
