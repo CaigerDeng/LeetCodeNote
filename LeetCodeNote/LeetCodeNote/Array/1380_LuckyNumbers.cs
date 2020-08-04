d5a19f2ce1869b508b3c3ea2cd771837b6f85b06
CaigerDeng <416715486@qq.com>
2020-08-04 11:52:43 +0800

    add 1380. lucky numbers

---
+++ LeetCodeNote/LeetCodeNote/Array/1380_LuckyNumbers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1380. 矩阵中的幸运数
+    /// https://leetcode-cn.com/problems/lucky-numbers-in-a-matrix/
+    /// </summary>
+
+
+    public class Solution_1380
+    {
+        // method 1
+        public IList<int> LuckyNumbers_0(int[][] matrix)
+        {
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            int[] rowMin = new int[rows];
+            for (int r = 0; r < rowMin.Length; r++)
+            {
+                rowMin[r] = int.MaxValue;
+            }
+            int[] colMax = new int[cols];
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    int v = matrix[r][c];
+                    rowMin[r] = Math.Min(rowMin[r], v);
+                    colMax[c] = Math.Max(colMax[c], v);
+                }
+            }
+            List<int> res = new List<int>();
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    int v = matrix[r][c];
+                    if (v == rowMin[r] && v == colMax[c])
+                    {
+                        res.Add(v);
+                    }
+                }
+            }
+            return res;
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
