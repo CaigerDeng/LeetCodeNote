3c1d41c4437296e4f409a34a656375140a0bc345
CaigerDeng <416715486@qq.com>
2020-08-06 14:30:27 +0800

    add 1389. create target array

---
+++ LeetCodeNote/LeetCodeNote/Array/1389_CreateTargetArray.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1389. 按既定顺序创建目标数组
+    /// https://leetcode-cn.com/problems/create-target-array-in-the-given-order/
+    /// </summary>
+
+    public class Solution_1389
+    {
+        // method 1
+        public int[] CreateTargetArray_0(int[] nums, int[] index)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                list.Insert(index[i], nums[i]);
+            }
+            return list.ToArray();
+
+        }
+
+
+
+
+
+    }
+
+
+}
