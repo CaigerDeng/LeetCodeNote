6660b166930ffffd23fd3b687f40977328c654ac
CaigerDeng <416715486@qq.com>
2020-08-17 15:21:12 +0800

    add 1464. max product

---
+++ LeetCodeNote/LeetCodeNote/Array/1464_MaxProduct.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1464. 数组中两元素的最大乘积
+    /// https://leetcode-cn.com/problems/maximum-product-of-two-elements-in-an-array/
+    /// </summary>
+
+    public class Solution_1464
+    {
+        // method 0
+        public int MaxProduct_0(int[] nums)
+        {
+            System.Array.Sort(nums);
+            return (nums[nums.Length - 1] - 1) * (nums[nums.Length - 2] - 1);
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
