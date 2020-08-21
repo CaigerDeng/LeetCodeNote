8f7e16f92054823fa0cb592aaa91314cc070a87a
CaigerDeng <416715486@qq.com>
2020-08-21 11:28:38 +0800

    add 1486. x or operation

---
+++ LeetCodeNote/LeetCodeNote/Array/1486_XorOperation.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1486. 数组异或操作
+    /// https://leetcode-cn.com/problems/xor-operation-in-an-array/
+    /// </summary>
+
+
+    public class Solution_1486
+    {
+        // method 0
+        public int XorOperation_0(int n, int start)
+        {
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                res ^= start + i * 2;
+            }
+            return res;
+
+        }
+
+
+    }
+}
