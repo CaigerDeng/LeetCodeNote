dcfe62eac6d0747d1a07020c22a4798c2e6bec18
CaigerDeng <416715486@qq.com>
2020-08-25 17:08:31 +0800

    add 1502. can make arithmetic progression

---
+++ LeetCodeNote/LeetCodeNote/Array/1502_CanMakeArithmeticProgression.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1502. 判断能否形成等差数列
+    /// https://leetcode-cn.com/problems/can-make-arithmetic-progression-from-sequence/
+    /// </summary>
+
+    public class Solution_1502
+    {
+        // method 0
+        public bool CanMakeArithmeticProgression_0(int[] arr)
+        {
+            System.Array.Sort(arr);
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] * 2 != arr[i - 1] + arr[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+    }
+}
