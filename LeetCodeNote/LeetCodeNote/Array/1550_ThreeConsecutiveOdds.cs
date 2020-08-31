163f3ccefe138e29d7add965f5015eecefc47540
CaigerDeng <416715486@qq.com>
2020-08-31 10:33:39 +0800

    add 1550. three consecutive odds

---
+++ LeetCodeNote/LeetCodeNote/Array/1550_ThreeConsecutiveOdds.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 1550. 存在连续三个奇数的数组
+    /// https://leetcode-cn.com/problems/three-consecutive-odds/
+    /// </summary>
+
+    public class Solution_1550
+    {
+        // method 0
+        public bool ThreeConsecutiveOdds_0(int[] arr)
+        {
+            for (int i = 0; i <= arr.Length - 3; i++)
+            {
+                if ((arr[i] & 1) == 1 && (arr[i + 1] & 1) == 1 & (arr[i + 2] & 1) == 1)  // 
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+    }
+
+
+}
