6356452d1b79a63d30f497e7639ac9eb815aaeb4
CaigerDeng <416715486@qq.com>
2020-09-07 19:20:12 +0800

    add 1572. diagonal sum

---
+++ LeetCodeNote/LeetCodeNote/Array/1572_DiagonalSum.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeNote.Array
+{
+
+    /// <summary>
+    /// 1572. 矩阵对角线元素的和
+    /// https://leetcode-cn.com/problems/matrix-diagonal-sum/
+    /// </summary>
+
+
+    public class Solution_1572
+    {
+        // method 0
+        public int DiagonalSum_0(int[][] mat)
+        {
+            int len = mat.Length;
+            int res = 0;
+            for (int i = 0; i < len; i++)
+            {
+                res += mat[i][i] + mat[i][len - 1 - i];
+            }
+            if (len % 2 == 1)
+            {
+                res -= mat[len / 2][len / 2];
+            }
+            return res;
+
+        }
+
+
+
+    }
+
+}
