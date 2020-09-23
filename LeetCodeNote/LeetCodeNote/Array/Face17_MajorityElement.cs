3a2e36f14c385ef0ae962189645d4b25701e68b0
CaigerDeng <416715486@qq.com>
2020-09-23 12:00:00 +0800

    add face17. majority element

---
+++ LeetCodeNote/LeetCodeNote/Array/Face17_MajorityElement.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCodeNote.Array
+{
+    /// <summary>
+    /// 面试题 17.10. 主要元素
+    /// https://leetcode-cn.com/problems/find-majority-element-lcci/
+    /// </summary>
+
+    public class Solution_Face17_10
+    {
+        // method 0
+        // 摩尔投票法，时间复杂度O(n)，空间复杂度O(1)
+        // https://leetcode-cn.com/problems/find-majority-element-lcci/solution/zhu-yao-yuan-su-by-jiao-jiao-bai-ju/
+        public int MajorityElement_0(int[] nums)
+        {
+            int major = 0;
+            int times = 0;
+            foreach (int x in nums)
+            {
+                if (times == 0)
+                {
+                    major = x;
+                    times++;
+                }
+                else
+                {
+                    if (major == x)
+                    {
+                        times++;
+                    }
+                    else
+                    {
+                        times--;
+                    }
+                }
+            }
+            if (times > 0)
+            {
+                int t = 0;
+                foreach (int x in nums)
+                {
+                    if (major == x)
+                    {
+                        t++;
+                    }
+                }
+                if (t > nums.Length / 2)
+                {
+                    return major;
+                }
+            }
+            return -1;
+
+        }
+
+
+    }
+
+
+
+}
