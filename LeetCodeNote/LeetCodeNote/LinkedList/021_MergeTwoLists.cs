9ee930b50c1bccac5a265072eb719420a895c6f3
CaigerDeng <416715486@qq.com>
2020-10-16 17:39:10 +0800

    add 021. merge two lists

---
+++ LeetCodeNote/LeetCodeNote/LinkedList/021_MergeTwoLists.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeNote.LinkedList
+{
+    /// <summary>
+    /// 21. 合并两个有序链表
+    /// https://leetcode-cn.com/problems/merge-two-sorted-lists/
+    /// </summary>
+
+    public class Solution_021
+    {
+
+
+
+        // method 1
+        public ListNode MergeTwoLists_1(ListNode l1, ListNode l2)
+        {
+            ListNode prehead = new ListNode(-1);
+            ListNode prev = prehead;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    prev.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }        
+            prev.next = l1 ?? l2;
+            // 返回的是真正的第一个节点，而不是返回哨兵节点
+            return prehead.next;
+
+        }
+
+
+    }
+
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+
+}
