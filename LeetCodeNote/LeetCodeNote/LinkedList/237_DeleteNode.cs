176d9307d7360e64bed53c84718481b1985b8ada
CaigerDeng <416715486@qq.com>
2020-11-03 19:34:40 +0800

    add 237. delete node

---
+++ LeetCodeNote/LeetCodeNote/LinkedList/237_DeleteNode.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeNote.LinkedList
+{
+    /// <summary>
+    /// 237. 删除链表中的节点
+    /// https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+    /// </summary>
+
+
+    public class Solution_237
+    {
+        // Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+
+        // method 0
+        public void DeleteNode_0(ListNode node)
+        {
+            // 没有head！
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+
+
+
+    }
+}
