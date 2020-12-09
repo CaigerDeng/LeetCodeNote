6b8a26b630059659e89b14b7fdb375b21a3d5914
CaigerDeng <416715486@qq.com>
2020-12-09 19:31:31 +0800

    add 1544. make good

---
+++ LeetCodeNote/LeetCodeNote/Stack/1544_MakeGood.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeNote.Stack
+{
+    /// <summary>
+    /// 1544. 整理字符串
+    /// https://leetcode-cn.com/problems/make-the-string-great/
+    /// </summary>
+
+
+    public class Solution_1544
+    {
+        // method 0
+        public string MakeGood_0(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                string str = s[i].ToString(); 
+                if (sb.Length > 0 && sb[sb.Length - 1].ToString().ToLower() == str.ToLower() && sb[sb.Length - 1].ToString() != str)
+                {
+                    sb.Remove(sb.Length - 1, 1);
+                }
+                else
+                {
+                    sb.Append(str);
+                }
+            }
+            return sb.ToString();
+
+        }
+
+
+    }
+
+}
