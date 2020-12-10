c3ef866f004055af3b32357f789db520da30928a
CaigerDeng <416715486@qq.com>
2020-12-10 19:32:28 +0800

    add 1598. min operations

---
+++ LeetCodeNote/LeetCodeNote/Stack/1598_MinOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCodeNote.Stack
+{
+
+    /// <summary>
+    /// 1598. 文件夹操作日志搜集器
+    /// https://leetcode-cn.com/problems/crawler-log-folder/
+    /// </summary>
+
+    public class Solution_1598
+    {
+        // method 0
+        public int MinOperations_0(string[] logs)
+        {
+            int step = 0;
+            for (int i = 0; i < logs.Length; i++)
+            {
+                string str = logs[i];
+                if (str == "../")
+                {
+                    step--;
+                    step = Math.Max(0, step);
+                }
+                else if(str == "./")
+                {
+                    
+                }
+                else
+                {
+                    step++;
+                }
+            }
+            return step;
+
+        }
+
+    }
+}
