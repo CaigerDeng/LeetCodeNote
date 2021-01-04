18e785b0fcc2d94790b53918a5fa66b2d4b89796
CaigerDeng <416715486@qq.com>
2021-01-04 19:18:05 +0800

    add 933. recent counter

---
+++ LeetCodeNote/LeetCodeNote/Queue/933_RecentCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeNote.Queue
+{
+    /// <summary>
+    /// 933. 最近的请求次数
+    /// https://leetcode-cn.com/problems/number-of-recent-calls/
+    /// </summary>
+
+
+    public class Solution_933_RecentCounter
+    {
+        // method 0
+        public class RecentCounter
+        {
+            private Queue<int> queue = null;
+
+            public RecentCounter()
+            {
+                queue = new Queue<int>();
+
+            }
+
+            public int Ping(int t)
+            {
+                queue.Enqueue(t);
+                while (queue.Peek() < t - 3000)
+                {
+                    queue.Dequeue();
+                }
+                return queue.Count;
+
+            }
+
+        }
+
+
+
+    }
+}
