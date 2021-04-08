9f658825fe91b505a5f3be20671bb72c02f750e5
CaigerDeng <416715486@qq.com>
2021-04-08 14:28:57 +0800

    add 884. uncommon from sentences

---
+++ LeetCodeNote/LeetCodeNote/HashTable/884_UncommonFromSentences.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeNote.HashTable
+{
+    /// <summary>
+    /// 884. 两句话中的不常见单词
+    /// https://leetcode-cn.com/problems/uncommon-words-from-two-sentences/
+    /// </summary>
+
+
+    public class Solution_884_UncommonFromSentences
+    {
+        // method 0
+        public string[] UncommonFromSentences(string A, string B)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            foreach (string word in A.Split(' '))
+            {
+                if (!dic.ContainsKey(word))
+                {
+                    dic.Add(word, 0);
+                }
+                dic[word]++;
+            }
+            foreach (string word in B.Split(' '))
+            {
+                if (!dic.ContainsKey(word))
+                {
+                    dic.Add(word, 0);
+                }
+                dic[word]++;
+            }
+            List<string> list = new List<string>();
+            foreach (KeyValuePair<string, int> pair in dic)
+            {
+                if (pair.Value == 1)
+                {
+                    list.Add(pair.Key);
+                }
+            }
+            return list.ToArray();
+
+        }
+
+    }
+
+}
