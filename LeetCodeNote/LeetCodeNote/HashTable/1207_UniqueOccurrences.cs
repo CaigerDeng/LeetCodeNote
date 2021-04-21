bcf3a474939164192c013e543ebcfd93afcc664c
CaigerDeng <416715486@qq.com>
2021-04-21 17:35:27 +0800

    add 1207. unique occurrences

---
+++ LeetCodeNote/LeetCodeNote/HashTable/1207_UniqueOccurrences.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeNote.HashTable
+{
+    /// <summary>
+    /// 1207. 独一无二的出现次数
+    /// https://leetcode-cn.com/problems/unique-number-of-occurrences/
+    /// </summary>
+
+
+    public class Solution_1207_UniqueOccurrences
+    {
+
+        // method 0
+        public bool UniqueOccurrences_0(int[] arr)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (int i in arr)
+            {
+                if (!dic.ContainsKey(i))
+                {
+                    dic.Add(i, 0);
+                }
+                dic[i]++;
+            }
+            HashSet<int> hs = new HashSet<int>();
+            foreach (int value in dic.Values)
+            {
+                if (hs.Contains(value))
+                {
+                    return false;
+                }
+                hs.Add(value);
+            }
+            return true;
+
+        }
+
+
+    }
+
+
+}
