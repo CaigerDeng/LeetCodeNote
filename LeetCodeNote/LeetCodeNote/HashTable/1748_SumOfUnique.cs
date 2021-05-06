2813986ce200e73ef988e5dd51aefd9c13e274fc
CaigerDeng <416715486@qq.com>
2021-05-06 19:18:55 +0800

    add 1748. sum of unique

---
+++ LeetCodeNote/LeetCodeNote/HashTable/1748_SumOfUnique.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeNote.HashTable
+{
+
+    /// <summary>
+    /// 1748. 唯一元素的和
+    /// https://leetcode-cn.com/problems/sum-of-unique-elements/
+    /// </summary>
+
+    public class Solution_1748_SumOfUnique
+    {
+        // method 0
+        public int SumOfUnique_0(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!dic.ContainsKey(num))
+                {
+                    dic.Add(num, 0);
+                }
+                dic[num]++;
+            }
+            int sum = 0;
+            foreach (KeyValuePair<int, int> pair in dic)
+            {
+                if (pair.Value == 1)
+                {
+                    sum += pair.Key;
+                }
+            }
+            return sum;
+
+        }
+
+    }
+}
