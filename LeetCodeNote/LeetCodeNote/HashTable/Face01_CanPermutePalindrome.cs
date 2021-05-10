321187e8133d1e036e1e50c738c4200ab503c7d4
CaigerDeng <416715486@qq.com>
2021-05-10 19:35:45 +0800

    add face01. can permute palindrome

---
+++ LeetCodeNote/LeetCodeNote/HashTable/Face01_CanPermutePalindrome.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace LeetCodeNote.HashTable
+{
+
+    /// <summary>
+    /// 面试题 01.04. 回文排列
+    /// https://leetcode-cn.com/problems/palindrome-permutation-lcci/
+    /// </summary>
+
+
+    public class Solution_Face01_CanPermutePalindrome
+    {
+        // method 0
+        // 没用哈希表，直接linq
+        // https://leetcode-cn.com/problems/palindrome-permutation-lcci/solution/hui-wen-pai-xu-jie-ti-si-lu-hui-wen-jiu-shi-zui-du/
+        public bool CanPermutePalindrome_0(string s)
+        {          
+            return s.ToList().GroupBy(i => i).Count(x => x.Count() % 2 == 1) < 2;
+
+        }
+
+    }
+}
